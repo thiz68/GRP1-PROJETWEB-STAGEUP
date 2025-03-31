a063179f2ed8d6291cdde0128e966a3991f87f16
Clery <65062914+lecreuseurfou@users.noreply.github.com>
2025-03-31 16:24:07 +0200

    Add files via upload

--- src/Models/StageUpModel.php
+++ src/Models/StageUpModel.php
@@ -37,9 +37,6 @@ class StageUpModel extends Model {
         $this->pdo = $this->connection->get_pdo();
     }
 
-
-
-
     public function getEntreprises($note_min=0.0) {
         try {
             $donnees = $this->pdo->query("
@@ -50,16 +47,147 @@ class StageUpModel extends Model {
         }
     }
 
-    public function getOffres($id_entreprise, $salaire_min=0) {
+    public function getOffres($id_entreprise=0, $salaire_min=0) {
         try {
-            $donnees = $this->pdo->query("
+            if ($id_entreprise == 0) { $donnees = $this->pdo->query("SELECT * from offers where offers.remun_offer >= ".$salaire_min.";"); }
+            else { $donnees = $this->pdo->query("
                 SELECT * from offers where offers.id_enterprise = ".$id_entreprise." 
-                and offers.remun_offer >= ".$salaire_min.";");
+                and offers.remun_offer >= ".$salaire_min.";"); }
             return $donnees->fetchAll();
         } catch (PDOException $message_erreur) {
             die("Erreur lors de la récupération des offres : " . $message_erreur->getMessage());
         }
     }
+
+    public function getEntreprise($id_entreprise=1) {
+        try {
+            $donnees = $this->pdo->query("
+                SELECT * from enterprises where enterprises.id_enterprise = ".$id_entreprise.";");
+            return $donnees->fetchAll();
+        } catch (PDOException $message_erreur) {
+            die("Erreur lors de la récupération des entreprises : " . $message_erreur->getMessage());
+        }
+    }
+
+    public function get_note_attribuee($id_utilisateur,$id_entreprise) {
+        try {
+            $donnees = $this->pdo->query("
+                SELECT rating_evaluate from evaluate where evaluate.id_enterprise = ".$id_entreprise."
+                AND evaluate.id_user = ".$id_utilisateur.";");
+            return $donnees->fetchAll();
+        } catch (PDOException $message_erreur) {
+            die("Erreur lors de la récupération de la note : " . $message_erreur->getMessage());
+        }
+    }
+
+    public function post_note_attribuee($id_utilisateur, $id_entreprise, $note_attribuee) {
+        try {
+            $requete = "INSERT INTO evaluate (id_user, id_enterprise, rating_evaluate)
+                    VALUES (:id_utilisateur, :id_entreprise, :note_attribuee)
+                    ON DUPLICATE KEY UPDATE rating_evaluate = :note_attribuee";
+    
+            $requete_prep = $this->pdo->prepare($requete);
+            $requete_prep->execute([
+                ':id_utilisateur' => $id_utilisateur,
+                ':id_entreprise' => $id_entreprise,
+                ':note_attribuee' => $note_attribuee
+            ]);
+        } catch (PDOException $message_erreur) {
+            die("Erreur lors de l'insertion de la note : " . $message_erreur->getMessage());
+        }
+    }
+
+
+    public function post_form_creer_pilote($nom, $prenom, $email, $mdp) {
+        try {
+            $requete = "INSERT INTO _user (firstname_user, lastname_user, email_user, password_user, id_rank)
+                    VALUES (:nom, :prenom, :email, :mdp, 2)";
+    
+            $requete_prep = $this->pdo->prepare($requete);
+            $requete_prep->execute([
+                ':nom' => $nom,
+                ':prenom' => $prenom,
+                ':email' => $email,
+                ':mdp' => $mdp,
+            ]);
+        } catch (PDOException $message_erreur) {
+            die("Erreur lors de l'insertion des donnees du compte: " . $message_erreur->getMessage());
+        }
+    }
+
+    public function post_form_creer_etudiant($nom, $prenom, $email, $mdp) {
+        try {
+            $requete = "INSERT INTO _user (firstname_user, lastname_user, email_user, password_user, id_rank)
+                    VALUES (:nom, :prenom, :email, :mdp, 3)";
+    
+            $requete_prep = $this->pdo->prepare($requete);
+            $requete_prep->execute([
+                ':nom' => $nom,
+                ':prenom' => $prenom,
+                ':email' => $email,
+                ':mdp' => $mdp,
+            ]);
+        } catch (PDOException $message_erreur) {
+            die("Erreur lors de l'insertion des donnees du compte: " . $message_erreur->getMessage());
+        }
+    }
+
+    public function post_form_creer_entreprise($nom, $description, $email, $tel) {
+        try {
+            $requete = "INSERT INTO enterprises (name_enterprise, description_enterprise, email_enterprise, tel_enterprise)
+                    VALUES (:nom, :description, :email, :tel)";
+    
+            $requete_prep = $this->pdo->prepare($requete);
+            $requete_prep->execute([
+                ':nom' => $nom,
+                ':description' => $description,
+                ':email' => $email,
+                ':tel' => $tel,
+            ]);
+        } catch (PDOException $message_erreur) {
+            die("Erreur lors de l'insertion des donnees du compte: " . $message_erreur->getMessage());
+        }
+    }
+
+
+
+
+    public function getUserByEmail($email) {
+        try {
+            $stmt = $this->pdo->prepare("SELECT * FROM _user WHERE email_user = :email LIMIT 1");
+            $stmt->execute([':email' => $email]);
+            return $stmt->fetch(PDO::FETCH_ASSOC);
+        } catch (PDOException $message_erreur) {
+            die("Erreur lors de la récupération de l'utilisateur : " . $message_erreur->getMessage());
+        }
+    }
+
+    public function getUserById($id) {
+        try {
+            $stmt = $this->pdo->prepare("SELECT * FROM _user WHERE id_user = :id LIMIT 1");
+            $stmt->execute([':id' => $id]);
+            return $stmt->fetch(PDO::FETCH_ASSOC);
+        } catch (PDOException $message_erreur) {
+            die ("Erreur lors de la récupération de l'utilisateur : " . $message_erreur->getMessage());
+        }
+    }
+
+    public function getUserWithRole($id) {
+        try {
+            $stmt = $this->pdo->prepare("
+            SELECT u.*, r.name_rank 
+            FROM _user u
+            JOIN ranks r ON u.id_rank = r.id_rank
+            WHERE u.id_user = :id 
+            LIMIT 1
+            ");
+            $stmt->execute([':id' => $id]);
+            return $stmt->fetch(PDO::FETCH_ASSOC);
+        } catch (PDOException $message_erreur) {
+            die("Erreur lors de la récupération de l'utilisateur et son rôle : " . $message_erreur->getMessage());
+        }
+    }
+
 }
 
 ?>
